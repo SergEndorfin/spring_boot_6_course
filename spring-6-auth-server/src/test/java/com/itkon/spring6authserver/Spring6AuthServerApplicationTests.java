c7179e23dd513b8c110c9926cb849578f3728357
SergEndorfin <38689177+SergEndorfin@users.noreply.github.com>
2023-08-08 22:53:16 +0300

    Add files via upload

---
+++ spring-6-auth-server/src/test/java/com/itkon/spring6authserver/Spring6AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkon.spring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
