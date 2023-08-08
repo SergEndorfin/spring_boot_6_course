474f2a361e5168b17c113e6401d8a1d1b3e6e40e
SergEndorfin <38689177+SergEndorfin@users.noreply.github.com>
2023-08-08 22:56:04 +0300

    Add files via upload

---
+++ spring-6-gateway/src/main/java/com/itkon/spring6gateway/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.itkon.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
